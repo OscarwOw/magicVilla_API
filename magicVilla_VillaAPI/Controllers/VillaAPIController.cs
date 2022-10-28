aa2e89b516934be4f2268142a5d19475ff2cf6b7
OscarwOw <OscarwOw210@gmail.com>
2022-10-28 22:54:35 +0200

    Create get villas endpoint

--- magicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ magicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using magicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace magicVilla_VillaAPI.Controllers
 {
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
-
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {ID=0,Name="chello"},
+                new Villa {ID=0,Name="chello"}
+            };
+        }
     }
 }
