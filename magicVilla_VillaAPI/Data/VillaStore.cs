476c874fbfdc32500bee502933941a98ce3c75ad
OscarwOw <OscarwOw210@gmail.com>
2022-10-29 12:24:32 +0200

    udpate data in storage

--- magicVilla_VillaAPI/Data/VillaStore.cs
+++ magicVilla_VillaAPI/Data/VillaStore.cs
@@ -5,8 +5,8 @@ namespace magicVilla_VillaAPI.Data
     public static class VillaStore
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>() {
-                new VillaDTO {ID=1,Name="chello"},
-                new VillaDTO {ID=2,Name="Janko"}
+                new VillaDTO {ID=1,Name="chello", Sqft = 100, Occupancy=4},
+                new VillaDTO {ID=2,Name="Janko",Sqft=120, Occupancy=3}
             };
     }
 }
